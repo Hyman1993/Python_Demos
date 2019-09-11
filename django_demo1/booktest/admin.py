383061605835912e543c465809f9dfbd99196974
Hyman <1392841128@qq.com>
2019-09-11 22:52:05 +0900

    2019.09.11 使用django进行后台管理

--- django_demo1/booktest/admin.py
+++ django_demo1/booktest/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
-
+import django.contrib
+from .models import *
 # Register your models here.
+
+from booktest.models import BookInfo, HeroInfo
+
+class BookInfoAdmin(django.contrib.admin.ModelAdmin):
+    list_display = ['id','title','pub_date']
+
+class HeroInfoAdmin(django.contrib.admin.ModelAdmin):
+    list_display = ['id','name','content','gender','book']
+
+django.contrib.admin.site.register(BookInfo,BookInfoAdmin)
+django.contrib.admin.site.register(HeroInfo,HeroInfoAdmin)
