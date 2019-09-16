0ce98bfe950ee1191d819a434e72a47d734b711c
Hyman <1392841128@qq.com>
2019-09-16 15:13:40 +0900

    2019.09.16  django的视图和模板的应用

--- django_demo1/booktest/views.py
+++ django_demo1/booktest/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+# HttpRequest
+def index(request):
+    # HttpResponse
+    context = {'title': 'django 首页', 'list': range(10)}
+    # 跳转到页面模板并传递参数
+    return render(request, 'booktest/index.html',context)
