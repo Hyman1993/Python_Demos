8b61c812a2d0ba97becf278857513eff669b07e7
Hyman <1392841128@qq.com>
2019-09-16 15:51:49 +0900

    2019.09.16 python_django开发的图书英雄app完成

--- django_demo1/booktest/urls.py
+++ django_demo1/booktest/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from booktest.views import index
+from booktest.views import index, detail
 
 urlpatterns = [
-    url('index/', index),
+    url('^$', index),
+    url('^(\d+)/$', detail),
 ]
--- django_demo1/booktest/views.py
+++ django_demo1/booktest/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 # HttpRequest
 def index(request):
     # HttpResponse
-    context = {'title': 'django 首页', 'list': range(10)}
+    # context = {'title': 'django 首页', 'list': range(10)}
     # 跳转到页面模板并传递参数
-    return render(request, 'booktest/index.html',context)
+    # return render(request, 'booktest/index.html',context)
+    list = BookInfo.objects.all()
+    context = {'booklist': list}
+    return render(request, 'booktest/index2.html', context)
+
+
+def detail(request, id):
+    list = BookInfo.objects.get(id=id).heroinfo_set.all()
+    context = {'herolist': list}
+    return render(request, 'booktest/detail.html', context)
