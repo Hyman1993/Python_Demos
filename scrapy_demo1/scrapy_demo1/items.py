be56b62e77bc2a8e9af7e72ab6be0b69cb2cf5c7
Hyman <1392841128@qq.com>
2019-09-21 15:00:32 +0900

    2019.09.21 python的scrapy爬虫框架demo

---
+++ scrapy_demo1/scrapy_demo1/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ScrapyDemo1Item(scrapy.Item):
+    # define the fields for your item here like:
+    # 老师姓名
+    name = scrapy.Field()
+    # 老师职称
+    title = scrapy.Field()
+    # 老师信息
+    info = scrapy.Field()
+    #pass
+
